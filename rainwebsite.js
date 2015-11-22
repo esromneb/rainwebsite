1a16e3571bfb74c104b7317a1beed2d98464a748
Ben <bjmorse@gmail.com>
2015-11-22 02:51:45 -0600

    first try, but it should all work

--- rainwebsite.js
+++ rainwebsite.js
@@ -1,45 +1,82 @@
+Log = new Mongo.Collection("log");
+Rain = new Mongo.Collection("rain");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
-  Router.route('/', function () {
-    this.render('hello', {
-      data: function () { }
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        },
+        rainstatus: function () {
+            var txt;
+            if( Rain.find().count() )
+            {
+                txt = "It's currently raining";
+            }
+            else
+            {
+                txt = "It's currently dry";
+            }
+            return txt;
+        }
     });
-  });
+
+    Template.hello.events({
+        'click #yes': function () {
+            Rain.insert({});
+        },
+        'click #no': function () {
+            while(Rain.find().count())
+            {
+                Rain.remove(Rain.findOne()._id);
+            }
+        }
+    });
+
+    Router.route('/', function () {
+        this.render('hello', {
+            data: function () { }
+        });
+    });
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-
-
-  Router.map(function () {
-    this.route('api', {
-      path: '/api',
-      where: 'server',
-      action: function () {
-        var json = {a: 4};
-        this.response.setHeader('Content-Type', 'application/json');
-        this.response.end(JSON.stringify(json));
-      }
+    Meteor.startup(function () {
+        // code to run on server at startup
     });
-  });
+
+
+    Router.map(function () {
+        this.route('api', {
+            path: '/api',
+            where: 'server',
+            action: function (a) {
+                Log.insert({query:a.query, time:new Date()});
+
+                foxid = a.query.id;
+                var obj = {};
+                if( Rain.find().count() )
+                {
+                    obj["foxid"] = {
+                        downlinkData: "0000000000000001"
+                    };
+                } else {
+                    obj["foxid"] = {
+                        downlinkData: "0000000000000000"
+                    };
+                }
+
+
+                //console.log(a.query);
+                //var json = {a: 4};
+                this.response.setHeader('Content-Type', 'application/json');
+                this.response.end(JSON.stringify(obj));
+            }
+        });
+    });
 
 
 }
